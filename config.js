559a39cb8adc99e58ea8eafb2d5ec67e0f91a1c0
yongjunfan0723 <fanyongjun1123@163.com>
2020-08-12 12:00:54 +0800

    updated config.js

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 const nodes = [
-  "https://seaaswtcrpc.jccdex.cn"
+  "http://159.138.246.146:3650",
+  "http://47.254.237.209:3650",
+  "http://47.254.242.111:3650",
+  "http://47.241.133.154:3650",
+  "http://47.241.16.210:3650",
 ];
 
 exports.nodes = nodes;
