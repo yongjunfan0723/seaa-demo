ddd186e79041d37f5bec9011eee732163e91a3b8
yongjunfan0723 <fanyongjun1123@163.com>
2020-08-14 16:26:35 +0800

    update config nodes

--- config.js
+++ config.js
@@ -3,7 +3,9 @@ const nodes = [
   "http://47.254.237.209:3650",
   "http://47.254.242.111:3650",
   "http://47.241.133.154:3650",
-  "http://47.241.16.210:3650",
+  "http://47.241.16.210:3650"
+  // "https://devseaaswtcrpc.jccdex.cn",
+  // "https://seaaswtcrpc.jccdex.cn"
 ];
 
 exports.nodes = nodes;
