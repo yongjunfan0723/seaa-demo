62adf0e664ba8863e02b75be0cdf0276e75f8985
yongjunfan0723 <fanyongjun1123@163.com>
2020-09-12 18:18:21 +0800

    change rpcNodes to hk nodes,In order to visit faster in the country

--- config.js
+++ config.js
@@ -1,10 +1,17 @@
 module.exports = {
   rpcNodes: [
-    "http://159.138.246.146:3650",
-    "http://47.254.237.209:3650",
-    "http://47.254.242.111:3650",
-    "http://47.241.133.154:3650",
-    "http://47.241.16.210:3650"
+    // "http://159.138.246.146:3650",
+    // "http://47.254.237.209:3650",
+    // "http://47.254.242.111:3650",
+    // "http://47.241.133.154:3650",
+    // "http://47.241.16.210:3650"
+    "https://srnhk147c59fb.seaaps.com",
+    "https://srnhk22f67a3c.seaaps.com",
+    // "https://srnmala147c59.seaaps.com",
+    // "https://srnmala2f69ee.seaaps.com",
+    // "https://srnmala34ce42.seaaps.com",
+    // "https://srnmala4f67a3.seaaps.com",
+    // "https://srnmala5c1ada.seaaps.com"
     // "https://devseaaswtcrpc.jccdex.cn",
     // "https://seaaswtcrpc.jccdex.cn"
   ],
